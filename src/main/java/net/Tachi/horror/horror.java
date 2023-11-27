5ee3a538bfb45b9ed77c6dba6d84bd7b2af1c60b
Tachibananekotawa <Buzzoof73@gmail.com>
2023-11-27 03:45:21 +0000

    Add the files

--- src/main/java/net/Tachi/horror/horror.java
+++ src/main/java/net/Tachi/horror/horror.java
@@ -1,6 +1,7 @@
 package net.Tachi.horror;
 
 import com.mojang.logging.LogUtils;
+import net.Tachi.horror.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -22,9 +23,10 @@ public class horror
     public horror()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        // Register the commonSetup method for modloading
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
-        // Register ourselves for server and other game events we are interested in
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 
--- src/main/java/net/Tachi/horror/item/ModItems.java
+++ src/main/java/net/Tachi/horror/item/ModItems.java
@@ -1,13 +1,21 @@
 package net.Tachi.horror.item;
 
+import net.Tachi.horror.horror;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static net.minecraft.world.item.CreativeModeTab.*;
+import static net.minecraft.world.item.Item.*;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, horror.MOD_ID);
+
+    public static final RegistryObject<Item> URANIUM = ITEMS.register("uranium",()-> new Item(new Item.Properties().tab(TAB_MISC)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
