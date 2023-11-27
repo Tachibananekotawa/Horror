091db8555449428b33adcfc45bf701a7d19d1cff
Tachibananekotawa <buzzoof73@gmail.com>
2023-11-27 03:26:48 +0000

    Add the files

--- src/main/java/net/Tachi/horror/item/ModItems.java
+++ src/main/java/net/Tachi/horror/item/ModItems.java
@@ -1,10 +1,15 @@
 package net.Tachi.horror.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
-    public static final DeferredRegister<Item> Items =
+    public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, horror.MOD_ID);
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
 }
