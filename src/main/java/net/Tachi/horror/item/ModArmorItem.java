dd436b208f3c59ed922dcdbffdf7831da6b7da15
Tachibananekotawa <buzzoof73@gmail.com>
2023-11-27 19:24:29 +0000

    Add the files

---
+++ src/main/java/net/Tachi/horror/item/ModArmorItem.java
@@ -0,0 +1,51 @@
+package net.Tachi.horror.item;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.eventbus.api.IEventBus;
+
+import static net.Tachi.horror.item.ModItems.ITEMS;
+import static net.Tachi.horror.item.ModItems.URANIUM_HELMET;
+
+public class ModArmorItem extends ArmorItem{
+
+    public ModArmorItem(ArmorMaterial armorMaterial, EquipmentSlot equipmentSlot, Properties properties) {
+
+        super(armorMaterial, equipmentSlot, properties);
+    }
+
+    @Override
+    public void onArmorTick(ItemStack stack, Level level, Player player) {
+        super.onArmorTick(stack, level, player);
+    }
+    private boolean hasFullSuitOfArmorOn(Player player) {
+        ItemStack boots = player.getInventory().getArmor(0);
+        ItemStack leggings = player.getInventory().getArmor(1);
+        ItemStack breastplate = player.getInventory().getArmor(2);
+        ItemStack helmet = player.getInventory().getArmor(3);
+
+        return !helmet.isEmpty() && !breastplate.isEmpty()
+                && !leggings.isEmpty() && !boots.isEmpty();
+    }
+    private boolean hasCorrectArmorOn(ArmorMaterial material, Player player) {
+        for (ItemStack armorStack : player.getInventory().armor) {
+            if (!(armorStack.getItem() instanceof ArmorItem)) {
+                return false;
+            }
+        }
+
+        ArmorItem boots = ((ArmorItem)player.getInventory().getArmor(0).getItem());
+        ArmorItem leggings = ((ArmorItem)player.getInventory().getArmor(1).getItem());
+        ArmorItem breastplate = ((ArmorItem)player.getInventory().getArmor(2).getItem());
+        IEventBus IEventBus = null;
+        ArmorItem helmet = ((ArmorItem)player.getInventory().getArmor(3).getItem());
+
+        return helmet.getMaterial() == material && breastplate.getMaterial() == material &&
+                leggings.getMaterial() == material && boots.getMaterial() == material;
+    }
+}
+
